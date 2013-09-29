875124566afbcdc30baa49277080850b6d413fdb
Nicolai <nicolai13@ru.is>
2013-09-29 18:17:59 +0000

    Added the code.

--- ggp-base/src/org/ggp/base/player/gamer/statemachine/sample/FabulousPlayer.java
+++ ggp-base/src/org/ggp/base/player/gamer/statemachine/sample/FabulousPlayer.java
@@ -1,6 +1,12 @@
 package org.ggp.base.player.gamer.statemachine.sample;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+import org.ggp.base.util.statemachine.MachineState;
 import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.StateMachine;
 import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
@@ -13,17 +19,94 @@ import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
  */
 public final class FabulousPlayer extends SampleGamer {
 	
-	private static Move best;
+	private Stack<Move> best = null;
+	
+	private Stack<Move> current = new Stack<Move>();
+	
+	private int bestScore = -1;
+	
+	private StateMachine theMachine = getStateMachine();
 	
 	@Override
 	public Move stateMachineSelectMove(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException{
-		//TODO: Implement
-		return null;
+		if(best == null || best.isEmpty()){
+			System.err.println("No moves.");
+			theMachine.getRandomMove(getCurrentState(), getRole());
+		}
+		return best.pop();
 	}
 	
 	@Override
 	public void stateMachineMetaGame(long timeout){
-		//TODO: Implement
+		timeout -= 500;
+		int depth = 4;
+		long now = System.currentTimeMillis();
+		long estimate;
+		while(! search(theMachine.getInitialState(), depth)){
+			estimate = 2 * (System.currentTimeMillis() - now);
+			now = System.currentTimeMillis();
+			if(now + estimate > timeout){
+				break;
+			}
+			depth++;
+		}
+		if(best == null){
+			return;
+		}
+		Stack<Move> copy = new Stack<Move>();
+		while(! best.isEmpty()){
+			copy.push(best.pop());
+		}
+		best = copy;
+	}
+	
+	/**
+	 * Limited depth first search.
+	 * 
+	 * @param state State to start the search from
+	 * @param depth Depth limit
+	 * @return True if further search is pointless
+	 */
+	private boolean search(MachineState state, int depth){
+		if(theMachine.isTerminal(state)){
+			int score = -1;
+			try {
+				score = theMachine.getGoal(state, getRole());
+			} catch (GoalDefinitionException e) {
+				System.err.println("Bad goal description!");
+				return true;
+			}
+			if(score > bestScore){
+				best = new Stack<Move>();
+				best.addAll(current);
+			}
+			return (score == 100);
+		}
+		if(depth == 0){
+			return false;
+		}
+		List<Move> moves;
+		try{
+			moves = theMachine.getLegalMoves(state, getRole());
+		} catch (MoveDefinitionException e){
+			System.err.println("No legal moves!");
+			return true;
+		}
+		for(Move m : moves){
+			current.push(m);
+			List<Move> list = new LinkedList<Move>();
+			list.add(m);
+			try {
+				if(search(theMachine.getNextState(state, list), --depth)){
+					return true;
+				}
+			} catch (TransitionDefinitionException e) {
+				System.err.println("Something went horribly wrong!");
+				System.exit(-1);
+			}
+			current.pop();
+		}
+		return false;
 	}
 	
 	@Override
