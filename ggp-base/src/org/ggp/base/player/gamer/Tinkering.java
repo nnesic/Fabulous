6984fe52fb2f4f9263c8551d7c96a6d708706461
Nera <nnesic@middlebury.edu>
2013-10-23 13:55:27 +0000

    Did thingy so now single player works again. Also have the javadocs.

---
+++ ggp-base/src/org/ggp/base/player/gamer/Tinkering.java
@@ -0,0 +1,18 @@
+package org.ggp.base.player.gamer;
+
+import java.util.Stack;
+
+public class Tinkering {
+
+	public static void main(String[] args) {
+		Stack <Integer> bla = new Stack <Integer> (); 
+		Stack <Integer> bla2 = new Stack <Integer> (); 
+		bla.push(0);
+		bla.push(1);
+		bla.push(2);
+		System.out.println(bla.remove(0));
+		System.out.println(bla.remove(0));
+		System.out.println(bla.remove(0));
+		
+	}
+}
--- ggp-base/src/org/ggp/base/player/gamer/statemachine/sample/FabulousPlayer.java
+++ ggp-base/src/org/ggp/base/player/gamer/statemachine/sample/FabulousPlayer.java
@@ -1,18 +1,59 @@
 package org.ggp.base.player.gamer.statemachine.sample;
 
+import java.util.List;
+
+import org.ggp.base.player.gamer.event.GamerSelectedMoveEvent;
+import org.ggp.base.player.gamer.exception.MetaGamingException;
 import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.StateMachine;
 import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
 
+/**
+ * 
+ * @author Nera
+ *
+ */
 public final class FabulousPlayer extends SampleGamer {
 
+	SampleGamer player;
+	
+	@Override
+	public void stateMachineMetaGame(long timeout){
+		StateMachine m = getStateMachine();
+		int roles = m.getRoles().size();
+		if (roles == 1){
+			player = new FabulousSinglePlayer();
+		}
+		else
+			player = new FabulousMultiPlayer();
+		player.setMatch(this.getMatch());
+		player.setRoleName(this.getRoleName());
+		try {
+			player.metaGame(timeout);
+		} catch (MetaGamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(roles);
+		System.out.println("OHAI");
+		
+	}
+	
 	@Override
 	public Move stateMachineSelectMove(long timeout)
 			throws TransitionDefinitionException, MoveDefinitionException,
 			GoalDefinitionException {
-		// TODO Auto-generated method stub
-		return null;
+		
+ 
+	/**	List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(), getRole());
+		Move selection = moves.get(0);*/
+
+		
+
+		//notifyObservers(new GamerSelectedMoveEvent(moves, selection, stop - start));
+		return player.stateMachineSelectMove(timeout);
 	}
 
 }
